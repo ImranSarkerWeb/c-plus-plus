dd7d422cb530ac2675522a762eaaee6ab070339f
imransarkerweb <imransarker.web@gmail.com>
2023-07-28 22:07:37 +0600

    area of circle

---
+++ intro/areaOfCircle.cpp
@@ -0,0 +1,13 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+int main()
+{
+    int radious;
+    double area;
+    cin >> radious;
+    area = 3.1416 * radious * radious;
+    cout << "The area of the circle=" << area << endl;
+
+    return 0;
+}
