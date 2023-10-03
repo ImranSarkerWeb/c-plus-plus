92ac027ee3dc915b66bf806762337214a3389922
imransarkerweb <imransarker.web@gmail.com>
2023-10-03 14:53:15 +0600

    linked list implemented

---
+++ linkedList.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+int main()
+{
+
+    Node *head = new Node(10);
+    Node *temp1 = new Node(20);
+    Node *temp2 = new Node(30);
+    head->next = temp1;
+    temp1->next = temp2;
+    cout << head->val << " " << head->next->val << " " << head->next->next->val << endl;
+
+    return 0;
+}
---
+++ youTube.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+
+    cout<<"What is your name?"<<endl;
+    string name;
+    cin>>name;
+    cout<<"Hi "<<name<<".\n";
+
+    return 0;
+}
